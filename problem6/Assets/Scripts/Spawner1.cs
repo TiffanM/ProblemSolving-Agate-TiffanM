a1153b3205f2eff6f0268b170224812772399f5d
TiffanM <tiffanmaulana04@gmail.com>
2021-10-10 20:31:18 +0700

    commit problem6

---
+++ problem6/Assets/Scripts/Spawner1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner1 : MonoBehaviour
+{
+    public GameObject Enemy;
+    public GameObject Ball;
+
+    void Start()
+    {
+        //buat si enemy kotak
+        for (int i = 0; i < 10; i++)
+        {
+            float spawnY = Random.Range
+                (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
+            float spawnX = Random.Range
+                (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+
+            Vector2 spawnPosition = new Vector2(spawnX, spawnY);
+            Instantiate(Enemy, spawnPosition, Quaternion.identity);
+        }
+        //ini buat bola
+        for (int i = 0; i < 1; i++)
+        {
+            float spawnY = Random.Range
+                (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
+            float spawnX = Random.Range
+                (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+
+            Vector2 spawnPosition = new Vector2(spawnX, spawnY);
+            Instantiate(Ball, spawnPosition, Quaternion.identity);
+        }
+    }
+}
